2ba30f6a567db451a71d37f312649422fea5b5ae
Ariel <arielord@buffalo.edu>
2018-12-23 07:56:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,14 @@
 class BankAccount
-  attr_accessor :status
-  attr_reader :name, :balance
+  attr_accessor :status, :balance
+  attr_reader :name
   
   def intialize(name)
     @name = name
-    @balance = 1000
+    self.balance = 1000
     self.status = "open"
   end
   
-  def deposit
-    
+  def deposit(money_deposit)
+    self.balance += money_deposit
   end
 end
